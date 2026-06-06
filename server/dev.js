093c3d2582930a10a0aae0e75f628b04e704fc5c
Sonic337 <sonicthesaga1@gmail.com>
2026-06-06 19:48:36 +0530

    Update dev.js

--- server/dev.js
+++ server/dev.js
@@ -1,8 +1,16 @@
 require('dotenv').config()
+const path = require('path')
 const app = require('./app')
 
 const PORT = process.env.PORT || 3001
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(require('express').static(path.join(__dirname, '../dist')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'))
+  })
+}
+
 app.listen(PORT, () => {
-  console.log(`API server running at http://localhost:${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
